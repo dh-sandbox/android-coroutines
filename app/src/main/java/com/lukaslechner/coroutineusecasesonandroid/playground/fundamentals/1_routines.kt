36d6911757f7b991b69edc849e559704a44b7508
Lukas Lechner <office@lukaslechner.com>
2026-03-02 17:55:07 +0100

    Remove files from playground module

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/fundamentals/1_routines.kt
@@ -0,0 +1,9 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.fundamentals
+
+// IMPORTANT: The playground functions stopped working with Android Studio Otter,
+// that's why I moved them from the "app" gradle module into a standalone Kotlin library
+// gradle module called playground.
+
+fun main() {
+
+}
--- playground/src/main/java/com/lukaslechner/playground/fundamentals/1_routines.kt
+++ playground/src/main/java/com/lukaslechner/playground/fundamentals/1_routines.kt
@@ -1,14 +1,4 @@
 package com.lukaslechner.playground.fundamentals
 
 fun main() {
-    println("main starts")
-    routine(1, 500)
-    routine(2, 300)
-    println("main ends")
-}
-
-fun routine(number: Int, delay: Long) {
-    println("Routine $number starts work")
-    Thread.sleep(delay)
-    println("Routine $number has finished")
 }
